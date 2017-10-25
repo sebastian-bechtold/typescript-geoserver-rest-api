147b45c8159bb9af7f9f1fb13816df310944abb0
Sebastian Bechtold <sbecht@geomer.de>
2017-10-25 11:15:13 +0200

    More promises conversion

--- src/GeoServerRestApi.ts
+++ src/GeoServerRestApi.ts
@@ -15,62 +15,78 @@ export class GeoServerRestApi {
     }
 
 
-    public loadLayerAsync(workspace: string, name: string, handler: any) {
-        let url = "/rest/layers/" + workspace + ":" + name + ".json";
+    public asyncLoadLayer(workspace: string, name: string) : Promise<any> {
 
-        this.asyncLoad(url).then((response: any) => { handler(response); });
-    }
+        return new Promise((resolve, reject) => {
+            let url = "/rest/layers/" + workspace + ":" + name + ".json";
 
-
-    public loadLayerGroupAsync(workspace: string, name: string, handler: any) {
-        let url = workspace == null ? "/rest/layergroups/" + name + ".json" : "/rest/workspaces/" + workspace + "/layergroups/" + name + ".json";
-
-        this.asyncLoad(url).then((response: any) => {
-
-            let argument = (response != null) ? response.layerGroup : null;
-            handler(argument);
+            this.asyncLoad(url).then((response: any) => { resolve(response); });
         });
     }
 
 
-    public loadLayerGroupListAsync(workspace: string, handler: any): any {
-        let url = workspace == null ? "/rest/layergroups.json" : "/rest/workspaces/" + workspace + "/layergroups.json";
+    public asyncLoadLayerGroup(workspace: string, name : string) : Promise<any> {
 
-        this.asyncLoad(url).then((response: any) => {
+        return new Promise((resolve, reject) => {
+            let url = workspace == null ? "/rest/layergroups/" + name + ".json" : "/rest/workspaces/" + workspace + "/layergroups/" + name + ".json";
 
-            if (typeof response.layerGroups.layerGroup !== "undefined") {
-                handler(response.layerGroups.layerGroup);
-            }
-            else {
-                handler([]);
-            }
+            this.asyncLoad(url).then((response: any) => {
+
+                let argument = (response != null) ? response.layerGroup : null;
+                resolve(argument);
+            });
         });
     }
 
 
-    public loadLayersAsync(handler: any): any {
-        let url = "/rest/layers.json";
+    public asyncLoadLayerGroupList(workspace: string): Promise<Array<any>> {
 
-        this.asyncLoad(url).then((response : any) => {
+        return new Promise((resolve, reject) => {
 
-            if (typeof response.layers.layer !== "undefined") {
-                handler(response.layers.layer);
-            }
-            else {
-                handler([]);
-            }
+            let url = workspace == null ? "/rest/layergroups.json" : "/rest/workspaces/" + workspace + "/layergroups.json";
+
+            this.asyncLoad(url).then((response: any) => {
+
+                if (typeof response.layerGroups.layerGroup !== "undefined") {
+                    resolve(response.layerGroups.layerGroup);
+                }
+                else {
+                    resolve([]);
+                }
+            });
         });
     }
 
 
-    loadWorkspacesAsync(handler: any) {
-        let url = "/rest/workspaces.json";
+    public loadLayersAsync(): Promise<Array<any>> {
 
-        this.asyncLoad(url).then( (response: any) => { handler(response.workspaces.workspace); });
+        return new Promise((resolve, reject) => {
+
+            let url = "/rest/layers.json";
+
+            this.asyncLoad(url).then((response: any) => {
+
+                if (typeof response.layers.layer !== "undefined") {
+                    resolve(response.layers.layer);
+                }
+                else {
+                    resolve([]);
+                }
+            });
+        });
     }
 
 
-    private asyncLoad(relUrl: string) {
+    loadWorkspacesAsync() : Promise<any> {
+
+        return new Promise((resolve, reject) => {
+            let url = "/rest/workspaces.json";
+            this.asyncLoad(url).then((response: any) => { resolve(response.workspaces.workspace); });
+        });
+    }
+
+
+    private asyncLoad(relUrl: string): Promise<any> {
 
         //################# BEGIN Promise definition ####################
         return new Promise((resolve, reject) => {
@@ -85,20 +101,20 @@ export class GeoServerRestApi {
             url += this.pGeoserverBaseUrl.toString() + relUrl;
             //######### END Build request URL ###########
 
-            let request : XMLHttpRequest = new XMLHttpRequest();
+            let request: XMLHttpRequest = new XMLHttpRequest();
 
 
             request.open("GET", url, true);
-            
+
             request.onload = () => {
                 resolve(JSON.parse(request.responseText));
             }
 
             request.onerror = () => {
                 console.error("HTTP request failed: " + request.statusText, request.responseText);
-                reject(null);
-            } 
-           
+                reject();
+            }
+
             request.send();
         });
         //################# END Promise definition ####################
